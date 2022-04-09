47b0dc041e400228e7697a46a3c0b6955593462b
chinatomuto <78049185+chinatomuto@users.noreply.github.com>
2022-04-09 23:58:32 +0100

    Initial Commit

---
+++ stalker walker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stalker walker
+//
+//  Created by nato on 09/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
