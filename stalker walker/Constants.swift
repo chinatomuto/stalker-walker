0d3fee8ac3e7b61b5cc301cb70dcda81fb3aaf22
chinatomuto <78049185+chinatomuto@users.noreply.github.com>
2022-04-10 01:20:24 +0100

    created UI

---
+++ stalker walker/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  stalker walker
+//
+//  Created by nato on 10/04/2022.
+//
+
+import Foundation
+
+struct k {
+    let instaToTwit = "instagramToTwitter"
+    let instaToTik = "instagramToTiktok"
+    let twitToInsta = "twitterToInstagram"
+    let twitToTik = "twitterToTiktok"
+    let tikToInsta = "tiktokToInstagram"
+    let tikToTwit = "tiktokToTwitter"
+}
